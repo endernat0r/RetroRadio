c01b2a930add1039ab0c9005ca6a06c2d0fdd152
Celal Kerem <Kitapdolubirhayat@gmail.com>
2026-07-27 00:47:27 +0300

    Added border, stop and play buttons and did minor fixes
    Added a neon border to the now playing area, learned to create a grid and did the stop and play buttons and also did some minor fixes.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from tkinter import font
+
 from RadioEngine import Engine
 import tkinter as tk
 from tkinter import Scale, StringVar
@@ -10,7 +12,9 @@ status_text.set("No station playing")
 
 engine = Engine()
 
-root.geometry("400x200")
+root.geometry("300x380")
+
+root.resizable(False, False)
 
 root.configure(bg="#121212")
 
@@ -30,9 +34,9 @@ def search_and_play_by_name():
     else:
         status_text.set("No station found for the given name.")
 
-tk.Label(root, text="Retro Radio", font=("Helvetica", 16), bg="#121212", fg="white").pack(pady=20)
+tk.Label(root, text="Retro Radio", font=("Helvetica", 20), bg="#121212", fg="white").pack(pady=20)
 
-tk.Label(root, textvariable=status_text, bg="#121212", fg="white").pack()
+tk.Label(root, font=("Courier", 12), borderwidth=2, relief="groove", bg="#0a2211", fg="#00ff66", textvariable=status_text).pack(ipadx=20, ipady=10, pady=10)
 
 entry = tk.Entry(root, width=20, bg="#333333", fg="white", insertbackground="white")
 entry.pack(pady=10)
@@ -43,8 +47,17 @@ button.pack()
 button2 = tk.Button(root, text="Listen to a station by entering a name", width=35, bg="#333333", fg="white", command=search_and_play_by_name)
 button2.pack()
 
+button_frame = tk.Frame(root, bg="#121212")
+button_frame.pack(pady=10)
+
+stopButton = tk.Button(button_frame, text="Stop", width=7, bg="#333333", fg="white", command=engine.stop_station)
+stopButton.pack(side=tk.LEFT, padx=10)
+
+resumeButton = tk.Button(button_frame, text="Resume", width=7, bg="#333333", fg="white", command=lambda: engine.play_station(engine.player.get_media().get_mrl()) if engine.player else None)
+resumeButton.pack(side=tk.LEFT, padx=10)
+
 scale = Scale(root, from_=0, to=100 , orient=tk.HORIZONTAL, label="Volume", bg="#333333", fg="white", command=lambda val: engine.set_volume(int(val)))
 scale.pack(pady=20)
-
+scale.set(70)
 
 root.mainloop()
