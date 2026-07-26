e85729238a6f354349c85799c63fe9d7df41b3ec
Celal Kerem <Kitapdolubirhayat@gmail.com>
2026-07-26 14:43:11 +0300

    Did tests and created an inital code
    Basically I spent most of my time learning about how this library works and how python works. After that I started to create some basic codes and this is the final one I created. Also I created the venv and initialized ssome librarys like pillow.

---
+++ apitest.py
@@ -0,0 +1,19 @@
+from pyradios import RadioBrowser, RadioFacets
+import vlc
+
+rb = RadioBrowser()
+
+rj = rb.search(tag="jazz")
+
+print(len(rj))
+
+print(rj[0])
+
+print(rj[0]["url_resolved"])
+
+vlc = vlc.MediaPlayer(rj[0]["url_resolved"])
+
+vlc.play()
+
+close = input("Press Enter to close the player...")
+
